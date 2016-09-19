bfdab68b6033e06aab2a7e49b7a3178ab931cd04
m <619322854@qq.com>
2016-09-19 21:04:16 +0800

    add two class

--- app/src/main/java/com/waterfairy/corner/utils/ImageInfoUtils.java
+++ app/src/main/java/com/waterfairy/corner/utils/ImageInfoUtils.java
@@ -1,24 +1,68 @@
 package com.waterfairy.corner.utils;
 
+import android.text.TextUtils;
+
 import com.waterfairy.corner.bean.ImageBean;
 
 import java.io.File;
 import java.util.LinkedList;
 import java.util.List;
 
+
 /**
  * Created by m on 2016/9/18.
  */
 public class ImageInfoUtils {
+    public  static  String ImageCollection="jpg"+"jpeg"+"png";
+    public  static  String VideoCollection ="mp4"+"3gp"+"avi"+"mkv";
     public static List<ImageBean> getFolderImager(String dirPath){
         List<ImageBean> images = new LinkedList<ImageBean>();
+        File dir = new File(dirPath);
+        File files[] = dir.listFiles();
+        if (files!=null){
+            for (File file:files){
+                if(file.isDirectory()){
+
+                }else {
+
+                }
+            }
+
+        }
+        else {
+            images = null;
+        }
+
+
 
         return images;
     }
 
-    public static List<File>  getFolderFile(){
-        List<File> files = new LinkedList<File>();
-        return files;
+    public static String getFileFormat(File file){
+     String name = file.getName();
+        String format = name.substring(name.lastIndexOf("."+1));
+        if (TextUtils.isEmpty(format)){
 
+        }
+        return   format;
+    }
+
+    public  boolean isImage(File file){
+        boolean isImage = false;
+        String format = getFileFormat(file);
+        if (ImageCollection.contains(format.toLowerCase())){
+            isImage = true;
+        }
+        return isImage;
+    }
+
+    public  boolean isVideo(File file){
+        boolean isVideo = false;
+        String format = getFileFormat(file);
+        if (VideoCollection.contains(format.toLowerCase())){
+            isVideo = true;
+        }
+        return isVideo;
     }
 }
+
